d078cc41c65ef46263f87ab63d0b303449eb18bd
Zinatoy <sg058043@gmail.com>
2022-06-16 23:34:08 +0600

    account/login, register

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,21 +5,29 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from apps.users.models import User
 
-# Create your views here.
+# Create your views here. 
 def register(request):
+ 
+    if request.user.is_authenticated:
+        return redirect('/tours')
+     
     if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
-            new_user = user_form.save(commit=False)
-            # Set the chosen password
-            new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
-            new_user.save()
-            return redirect('index')
+        form = UserRegistrationForm(request.POST)
+ 
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username,password = password)
+            login(request, user)
+            return redirect('/tours')
+         
+        else:
+            return render(request,'account/register.html',{'form':form})
+     
     else:
-        user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form': user_form})
+        form = UserRegistrationForm()
+        return render(request,'account/register.html',{'form':form})
 
 def login(request):
     if request.method == 'POST':
