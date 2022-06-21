c23b03db4458b99d787e1104911da212d38448fc
Zinatoy <sg058043@gmail.com>
2022-06-21 22:56:34 +0600

    search

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from apps.settings.models import Setting,Facilities,About,Partners
 from apps.tours.models import Tour, Benefits
 from apps.staff.models import Staff
-
+from django.db.models import Q
 # Create your views here.
 def index(request):
-    home = Setting.objects.latest('id')
-    tours = Tour.objects.all()
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        tours = Tour.objects.filter(Q(title__icontains=search_query)| Q(description__icontains=search_query))
+    else:
+        tours = Tour.objects.all
+
+    home = Setting.objects.latest('id') 
     staff = Staff.objects.all()
     benefits = Benefits.objects.all()
     facilities = Facilities.objects.all()
